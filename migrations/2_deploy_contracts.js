f0ff3afa129598746c6254b118f2453e281dec20
Gianni Settino <g@miltonandparc.com>
2017-09-16 23:31:32 -0400

    Adds NFLTeams contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var NFL = artifacts.require("./NFL.sol");
+var NFLTeams = artifacts.require('./NFLTeams.sol');
 
 module.exports = function(deployer) {
   deployer.deploy(NFL, 16);
+  deployer.deploy(NFLTeams);
 };
